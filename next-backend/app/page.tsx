025373461bac2ae36fca4507af7d98da9681c388
aayushk9 <ayushkokate1@gmail.com>
2025-03-26 00:05:47 +0530

    initial commit

---
+++ next-backend/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import axios from "axios";
+
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+  return response.data
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+  return (
+    <div className="">
+      Hola     
+      <br></br>
+    {userData.name}
+    {userData.email}   
+    </div>
+  );
+}
