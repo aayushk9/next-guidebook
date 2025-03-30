d542eba096772a2ca2d3410ee53952f30ef1b7f2
aayushk9 <ayushkokate1@gmail.com>
2025-03-30 21:14:32 +0530

    next app

--- form-with-db/app/api/user/route.ts
+++ form-with-db/app/api/user/route.ts
@@ -1,10 +1,21 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+
+const client = new PrismaClient();
+
 
 export async function POST(req: NextRequest) {
    const body = await req.json();
 
-   return NextResponse.json({
-    body
-   })
-}
 
+   await client.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+      },
+   });
+
+   return NextResponse.json({ body });
+
+}
